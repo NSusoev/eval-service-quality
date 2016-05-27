301ca1139b1a5e94551ce6d9177b2b64b56f5556
NSusoev <susoev.nikita@gmail.com>
2016-05-27 10:39:12 +0300

    #14 Реализовать алгоритм до этапа разбиения исходных данных на однородные группы
    Реализовал метод для выборки оценок качества для подкритерия по заданной оценке важности

--- src/main/java/esq/application/repository/LinguisticTermRepository.java
+++ src/main/java/esq/application/repository/LinguisticTermRepository.java
@@ -1,10 +1,28 @@
 package esq.application.repository;
 
 import esq.application.model.LinguisticTerm;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by nsusoev on 07.05.16.
  */
 public interface LinguisticTermRepository extends CrudRepository<LinguisticTerm, Long>{
+
+    @Query(value = "SELECT t.id, t.name" +
+            " FROM service_quality_surveys s, service_quality_survey_results r, linguistic_terms t" +
+            " WHERE s.id = r.service_quality_survey_id " +
+                "and r.importance_mark_id = t.id " +
+                "and s.client_category_id = ?1 " +
+                "and s.client_group_id = ?2 " +
+                "and s.service_id = ?3 " +
+                "and r.service_quality_criteria_id = ?4 " +
+                "and r.importance_mark_id = ?5", nativeQuery = true)
+    List<LinguisticTerm> findQualityMarksForGroup(Long clientCategoryId,
+                                                  Long clientGroupId,
+                                                  Long serviceId,
+                                                  Long serviceQualityCriteriaId,
+                                                  Long importanceMarkId);
 }
