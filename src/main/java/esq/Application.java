8840ceacc0462359e0490f451bc6c456ea92e115
NSusoev <susoev.nikita@gmail.com>
2016-05-27 10:24:55 +0300

    Переместил класс Application.java в корневой пакет

--- src/main/java/esq/Application.java
+++ src/main/java/esq/Application.java
@@ -1,4 +1,4 @@
-package esq.application;
+package esq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/esq/core/ESQCalculator.java
+++ src/main/java/esq/core/ESQCalculator.java
@@ -1,24 +1,79 @@
 package esq.core;
 
-import esq.application.model.ESQSettingsProfile;
-import esq.application.model.ServiceQualitySurvey;
+import esq.application.model.*;
+import esq.application.repository.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 /**
  * Класс расчёта интегральной оценки качества
  */
 
 
+@Service
 public class ESQCalculator {
 
     private ESQSettingsProfile settingsProfile;
+    private static final Logger log = LoggerFactory.getLogger(ESQCalculator.class);
+    @Autowired
+    private LinguisticTermRepository linguisticTermRepository;
+    @Autowired
+    private ClientCategoryRepository clientCategoryRepository;
+    @Autowired
+    private ClientGroupRepository clientGroupRepository;
+    @Autowired
+    private ServiceRepository serviceRepository;
+    @Autowired
+    private ServiceQualityCriteriaRepository serviceQualityCriteriaRepository;
+
+    public ESQCalculator() {
+
+    }
 
     public ESQCalculator(ESQSettingsProfile settingsProfile) {
         this.settingsProfile = settingsProfile;
     }
 
+    public void setSettingsProfile(ESQSettingsProfile settingsProfile) {
+        this.settingsProfile = settingsProfile;
+    }
+
+    public ESQSettingsProfile getSettingsProfile() {
+        return settingsProfile;
+    }
+
+    public List<ESQSurveyResultGroup> getGroupedSurveyResults() {
+        log.debug("ENTER");
+        List<ESQSurveyResultGroup> resultGroups = new ArrayList<>();
+
+        for (ClientCategory category : clientCategoryRepository.findAll()) {
+            for (ClientGroup group : category.getClientGroups()) {
+                for (Service service : category.getServicesForThisCategory()) {
+                    for (ServiceQualityCriteria criteria : service.getServiceQualityCriterias()) {
+                        for (LinguisticTerm importance : linguisticTermRepository.findAll()) {
+                            List<LinguisticTerm> qualityMarks = linguisticTermRepository.findQualityMarksForGroup(
+                                    category.getId(),
+                                    group.getId(),
+                                    service.getId(),
+                                    criteria.getId(),
+                                    importance.getId());
+                            if (qualityMarks != null) {
+                                resultGroups.add(new ESQSurveyResultGroup(category, group, service, criteria, importance, qualityMarks));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        log.debug("created groups = {}", resultGroups);
+        log.debug("EXIT");
+        return resultGroups;
+    }
 }
